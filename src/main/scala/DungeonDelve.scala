f8efe3da12cdbae2a788578b37c476727fd410dc
Bilal-Khan95 <BackTrack1984@hotmail.com>
2019-01-09 10:10:23 +0000

    Death Run and the exercises

--- src/main/scala/DungeonDelve.scala
+++ src/main/scala/DungeonDelve.scala
@@ -2,71 +2,135 @@ class DungeonDelve {
  var overallDead = false
 }
 
-trait TheDoEverything{
+trait TheDoEverything extends Dice with Items{
 
-  var Strength:Int
-  var Dexterity:Int
-  var Constitution:Int
-  var Intelligence:Int
-  var Wisdom:Int
-  var Charisma:Int
-  //def attack={}
+  var Strength:Int = 0
+  var Dexterity:Int  = 0
+  var Constitution:Int  = 0
+  var Intelligence:Int  = 0
+  var Wisdom:Int  = 0
+  var Charisma:Int  = 0
+
+  def Combat(playerNADH:Tuple4[String,Tuple3[Int,Int,String],Int,Int],enemyNADH:Tuple4[String,Tuple3[Int,Int,String],Int,Int])={
+    println(s"${playerNADH._1} is under attack by ${enemyNADH._1}")
+    //1time instantiating variables
+    var ehealth = enemyNADH._4
+    var phealth = playerNADH._4
+    var combat = true
+    //Attack phase => +ve is player does damage, -ve is enemy damage
+    //multi-instantiating variables
+    while (combat){
+      var dmg = 0
+      var pattack = rollDice((playerNADH._2._1,playerNADH._2._2),weapons(playerNADH._2._3))
+      var eattack = rollDice((enemyNADH._2._1,enemyNADH._2._2),weapons(enemyNADH._2._3))
+      var attack = pattack-eattack
+
+      if (attack> 0){//Player hits
+        dmg = attack - enemyNADH._3
+        if (dmg > 0){
+          ehealth - dmg
+        }
+      }
+      else if (attack < 0){
+        dmg = attack + playerNADH._3
+        if (dmg > 0){
+          phealth - dmg
+        }
+
+      }
+
+      //Resolve round
+
+
+
+    }
+  }
  // def return_stats={}
 }
+
+trait Items {
+  var weapons:Map[String,Int]= Map("Sword"->2,"Club"->0,"Dagger"->1).withDefaultValue(0)
+}
+
 trait Inventory{
 }
 trait Magic{}
 trait Dice{
   var randInt = scala.util.Random
-  def rollDice(numberOf:Int,size:Int,bonus:Int=0):Int={
-    return (randInt.nextInt((numberOf*size)-1)+numberOf+bonus)
+  def rollDice(diceTuple:Tuple2[Int,Int],bonus:Int=0):Int={
+    return (randInt.nextInt((diceTuple._1*diceTuple._2)-1)+diceTuple._1+bonus)
   }
 }
 
-trait RacialBonuses{
-  var str:Int
-  var dex:Int
-  var con:Int
-  var intel:Int
-  var wis:Int
-  var cha:Int
+abstract class RacialBonuses{
+  var str: Tuple2[Int,Int]
+  var dex: Tuple2[Int,Int]
+  var con: Tuple2[Int,Int]
+  var intel: Tuple2[Int,Int]
+  var wis: Tuple2[Int,Int]
+  var cha: Tuple2[Int,Int]
 
 }
-class Elf extends RacialBonuses {
-  str = (1,6)
-  dex = (1,10)
-  con = (2,8)
-  intel = (1,8)
-  wis = (1,6)
-  cha = (1,8)
+//class Dwarf extends RacialBonuses{}
+case class Elf(
+                var str: Tuple2[Int,Int],
+                var dex: Tuple2[Int,Int],
+                var con: Tuple2[Int,Int],
+                var intel: Tuple2[Int,Int],
+                var wis: Tuple2[Int,Int],
+                var cha: Tuple2[Int,Int]
+) extends RacialBonuses {
+  //str = (1,6)
+  //dex = (1,10)
+  //con = (2,8)
+  //intel = (1,8)
+  //wis = (1,6)
+  //cha = (1,8)
 
 }
-class MundaneElf extends TheDoEverything with Inventory with Dice{
-  var race = new Elf()
-  var Strength = 0
-  var Dexterity = 0
-  var Constitution = 0
-  var Intelligence = 0
-  var Wisdom = 0
-  var Charisma = 0
+
+case class Human(var str: Tuple2[Int,Int],
+                 var dex: Tuple2[Int,Int],
+                 var con: Tuple2[Int,Int],
+                 var intel: Tuple2[Int,Int],
+                 var wis: Tuple2[Int,Int],
+                 var cha: Tuple2[Int,Int]) extends RacialBonuses{
+//  str = (1,6)
+//  dex = (1,6)
+//  con = (2,6)
+//  intel = (1,6)
+//  wis = (1,6)
+//  cha = (1,6)
+}
+
+
+abstract class Mundane extends TheDoEverything  with Dice  with Inventory {
+
+  var race = Elf((1,6), (1,6), (1,6), (1,6), (1,6), (1,6))
   var name = ""
 
   def createCharacterAttributes() = {
-    this.name = readLine("What are you called brave idiot?")
-    this.Strength = rollDice(1,race.str)
-    this.Dexterity = rollDice(1,race.dex)
-    this.Constitution = rollDice(2,race.con._2)
-    this.Intelligence = rollDice(1,race.intel._2)
-    this.Wisdom = rollDice(1,8)
-    this.Charisma = rollDice(1,8)
+    name = readLine("What are you called brave idiot?")
+    Strength = rollDice((race.str._1,race.str._2))
+    Dexterity = rollDice((race.dex._1,race.dex._2))
+    Constitution = rollDice((race.con._2,race.con._2))
+    Intelligence = rollDice((race.intel._1,race.intel._2))
+    Wisdom = rollDice((race.wis._1,race.wis._2))
+    Charisma = rollDice((race.cha._1,race.cha._2))
   }
-  createCharacterAttributes()
 }
-//class Mundane extends Character{}
+class MundaneElf extends Mundane{
+   race = Elf((1,6), (1,6), (1,6), (1,6), (1,6), (1,6))
+
+}
+//class MundaneDwarf extends Mundane{
+//  override val race = new Dwarf()
+//}
 //object initialiser{
 //}
 object DungeonDelveGame extends App{
   var ElfChara = new MundaneElf()
+  ElfChara.createCharacterAttributes()
   println(ElfChara.Strength)
 
 }
