5ccf6e94a1adfaf9015f20b3f23fee6029107ec0
Bilal-Khan95 <BackTrack1984@hotmail.com>
2019-01-10 17:32:37 +0000

    Death Run and the exercises

--- src/main/scala/Lumberjack.scala
+++ src/main/scala/Lumberjack.scala
@@ -1,3 +1,43 @@
-class Lumberjack {
+object Lumberjack extends App{
+  var randInt = scala.util.Random
+
+  def createLogStorage(size:Int):Array[Int]={
+    val logStorage =  Array.fill(size*size+1){randInt.nextInt(4)+1}
+//    for (k <- 0 until (size*size)){
+//      logStorage(k) =(randInt.nextInt(4)+1)
+//    }
+    return logStorage
+  }
+
+  def fillLogStorage(logs:Int,logStorage:Array[Int]):Array[Int]={
+    var logsLeft = logs
+    var smallestPile = logStorage.min
+    while (logsLeft > 0){
+      for(k <- 0 until logStorage.length){
+        if (logStorage(k) == smallestPile){
+          logStorage(k) += 1
+          logsLeft -= 1
+          smallestPile = logStorage.min
+        }
+      }
+    }
+    return logStorage
+  }
+
+  def printLogStorage(size:Int,logStorage:Array[Int])={
+    for (k <- 1 to size*size){
+      print(logStorage(k)+",")
+      if (k%3 == 0){
+        println("")
+      }
+    }
+  }
+
+  def doItAll(size:Int,numberOfLogs:Int): Unit ={
+    var logStorage = fillLogStorage(numberOfLogs,createLogStorage(size))
+    printLogStorage(size,logStorage)
+  }
+
+  doItAll(3,7)
 
 }
